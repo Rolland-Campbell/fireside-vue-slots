4166fa5cd67bc3f05516ef64a150f3a02c6ffba0
Rolland <Rollandcampbell@msn.com>
2019-12-17 16:50:39 -0700

    added another modal with a card and title, done

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <!-- brought back title withe named slot property, simple styling -->
-    <button class="btn btn-danger" data-toggle="modal" data-target="#three">Modal three</button>
-    <ModalOne id="three">
-      <div slot="title" class="modalThree">
-        <i class="fas fa-dragon mr-3"></i>I brought back the title!
-      </div>
-      <h5>Same modal, even more stuff</h5>
-      <br />
-      <!-- added a change to the modal layout -->
-      <input class="mt-5" type="text" placeholder="Input field?.. what the..." />
-    </ModalOne>
+    <div class="row justify-content-center">
+      <!-- brought back title withe named slot property, same modal, simple styling -->
+      <button class="btn btn-danger" data-toggle="modal" data-target="#three">Modal three</button>
+      <ModalOne id="three">
+        <div slot="title" class="modalThree">
+          <i class="fas fa-dragon mr-3"></i>I brought back the title!
+        </div>
+        <h5>Same modal, even more stuff</h5>
+        <br />
+        <!-- added an input to the modal layout -->
+        <input class="mt-5" type="text" placeholder="Input field?.. what the..." v-model="message" />
+      </ModalOne>
+    </div>
+    <div>
+      <p class="mt-5">{{ message }}</p>
+    </div>
+    <h2>Not ANOTHER modal!!</h2>
+    <div class="row justify-content-center">
+      <button class="btn btn-info" data-toggle="modal" data-target="#four">Modal four</button>
+      <!-- bringing in modal again, adding title and a card to it -->
+      <ModalOne id="four">
+        <div slot="title">
+          <h3>More Stuff</h3>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="../assets/robot.png" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Cool Card</h5>
+            <p class="card-text">I used the same modal, but added a card!</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </ModalOne>
+    </div>
   </div>
 </template>
 
@@ -20,7 +43,9 @@ import ModalOne from "../components/Modal-One";
 export default {
   name: "about",
   data() {
-    return {};
+    return {
+      message: ""
+    };
   },
   computed: {},
   methods: {},
